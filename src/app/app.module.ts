6ddea6f44f1fde98e0ade52fd18a251ed3b22a1a
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2022-03-29 09:32:39 -0300

    feature/ add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { rootRouterConfig } from './app.router';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './create/create.component';
 import { UpdateComponent } from './update/update.component';
 import { DeleteComponent } from './delete/delete.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +26,15 @@ import { DeleteComponent } from './delete/delete.component';
     DeleteComponent
   ],
   imports: [
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
     BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
     [RouterModule.forRoot(rootRouterConfig, {useHash: true})]
   ],
   providers: [
